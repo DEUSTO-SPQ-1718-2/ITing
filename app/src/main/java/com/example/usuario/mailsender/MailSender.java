a9803a3f6d80e05f37efa7041240a4af6ca63917
asierelorza <asier.elorza@opendeusto.es>
2017-10-16 17:17:27 +0200

    Implementación inicial del envío de mail. Cosas pendientes: - Que no se tenga que bajar el nivel en Gmail (para el caso del emisor) - Modificar el cuerpo para que el mensaje sea más completo y que incorpore el código generado - Integrarlo con las pantallas correspondientes

---
+++ app/src/main/java/com/example/usuario/mailsender/MailSender.java
@@ -0,0 +1,27 @@
+package com.example.usuario.mailsender;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by axi_e on 15/10/2017.
+ */
+
+public class MailSender {
+
+
+    public MailSender(){}
+
+    public boolean funciona(){
+
+        try{
+            GMailSender sender = new GMailSender("noreply.iting@gmail.com", "ItingPrueba14");
+            sender.sendMail("Asunto", "<b>Cuerpoo</b>", "noreply.iting@gmail.com", "asier.elorza@opendeusto.es,axi.elorza15@gmail.com");
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
