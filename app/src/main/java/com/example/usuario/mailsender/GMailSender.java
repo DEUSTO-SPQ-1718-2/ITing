bc8cbec088d8ef51ef86c9a5b40ff75e25ce18f4
asierelorza <asier.elorza@opendeusto.es>
2017-10-19 18:40:52 +0200

    Modificado el contenido del mensaje incorporando formato en HTML. Falta adjuntar los atributos correspondientes al contenido del mensaje + imagen.

--- app/src/main/java/com/example/usuario/mailsender/GMailSender.java
+++ app/src/main/java/com/example/usuario/mailsender/GMailSender.java
@@ -1,6 +1,7 @@
 package com.example.usuario.mailsender;
 
 import android.accounts.AccountManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import java.security.Security;
 import java.util.Properties;
 
 import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
@@ -18,7 +22,9 @@ import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
 
 /**
@@ -27,65 +33,70 @@ import javax.mail.util.ByteArrayDataSource;
 
 public class GMailSender extends javax.mail.Authenticator {
 
+    private String mailhost = "smtp.gmail.com";
+    private String user;
+    private String password;
     private Session session;
 
-    public GMailSender(String user){
+    static {
+        Security.addProvider(new JSSEProvider());
+    }
 
-//        this.user = user;
-//        this.password = password;
-//
-//        Properties props = new Properties();
-//        props.setProperty("mail.transport.protocol", "smtp");
-//        props.setProperty("mail.host", mailhost);
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.port", "465");
-//        props.put("mail.smtp.socketFactory.port", "465");
-//        props.put("mail.smtp.socketFactory.class",
-//                "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.smtp.socketFactory.fallback", "false");
-//        props.setProperty("mail.smtp.quitwait", "false");
-//
-//        session = Session.getDefaultInstance(props, this);
+    public GMailSender(String user, String password){
+
+        this.user = user;
+        this.password = password;
 
         Properties props = new Properties();
-        props.setProperty("mail.transport.protocol", "imap");
-        props.setProperty("mail.host", "imap.gmail.com");
-        props.put("mail.imap.port", "993");
-        props.put("mail.imap.ssl.enable", "true"); // required for Gmail
-        props.put("mail.imap.auth.mechanisms", "XOAUTH2");
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", mailhost);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.quitwait", "false");
 
-        session = Session.getInstance(props);
-        Store store = null;
-        try {
-            store = session.getStore("imap");
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        }
+        session = Session.getDefaultInstance(props, this);
 
-        if(store != null){
 
-            try {
-                store.connect("imap.gmail.com", user, "ya29.GlvnBPRDHOehywT7kH3Y4LzoriMVb2jajuP-XdqHwwE3ZvOFblkIEhYUKHSHFRduXdptxsAtYHsJh-xUrGb27-1jWQpm-uazU5g_nEFycTjvSLR3MhpxiskjBcFN");
-                // Token expired, need to refresh it
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-        }
+    }
 
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(user, password);
     }
 
     public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception {
         try{
             MimeMessage message = new MimeMessage(session);
-            DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/html"));
+            // DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/html"));
             message.setSender(new InternetAddress(sender));
             message.setSubject(subject);
-            message.setDataHandler(handler);
+           // message.setDataHandler(handler);
             if(recipients.indexOf(',') > 0){
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
             } else {
                 message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
             }
+
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(body, "text/html");
+
+            multipart.addBodyPart(messageBodyPart);
+
+//            messageBodyPart = new MimeBodyPart();
+//            DataSource fds = new FileDataSource();
+//
+//            messageBodyPart.setDataHandler(new DataHandler(fds));
+//            messageBodyPart.setHeader("Iting", "<image>");
+//
+//            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+
             Transport.send(message);
         } catch(Exception e){
             e.printStackTrace();
--- app/src/main/java/com/example/usuario/mailsender/MailSender.java
+++ app/src/main/java/com/example/usuario/mailsender/MailSender.java
@@ -1,5 +1,6 @@
 package com.example.usuario.mailsender;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,8 +16,18 @@ public class MailSender {
     public boolean funciona(){
 
         try{
-            GMailSender sender = new GMailSender("noreply.iting@gmail.com");
-            sender.sendMail("Asunto", "<b>Cuerpoo</b>", "noreply.iting@gmail.com", "asier.elorza@opendeusto.es,axi.elorza15@gmail.com");
+            GMailSender sender = new GMailSender("noreply.iting@gmail.com", "ItingPrueba14");
+
+            String body = "<div><img src=\"cid:image\"></div>" +
+                    "<div><h1><strong>&iexcl;Hola, nombre eCamarero!</strong></h1>\n" +
+                    "<p><span style=\"font-size:16px;\">nombre emisor te ha asignado como eCamarero en el restaurante x.</span></p>\n" +
+                    "<p><span style=\"font-size:16px;\">El <strong>c&oacute;digo de reserva</strong> es el siguiente: <strong>c&oacute;digo de reserva</strong></span></p>\n" +
+                    "<p><span style=\"font-size:16px;\">&iexcl;Corre a confirmar tu asistencia a la app de Iting!</span></p>\n" +
+                    "<p><span style=\"font-size:16px;\">Atentamente,</span></p>\n" +
+                    "<p><span style=\"font-size:16px;\">---------</span></p>\n" +
+                    "<p><em><span style=\"font-size:16px;\">Equipo de Iting</span></em></p></div>";
+
+            sender.sendMail("[Iting] nombre del emisor te ha invitado a comer", body, "noreply.iting@gmail.com", "asier.elorza@opendeusto.es");
             return true;
         } catch (Exception e){
             e.printStackTrace();
